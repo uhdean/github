221587e6cefdd810131df68f5d2750c256b9573d
uhdean <uhdean13@outlook.kr>
2022-03-17 11:12:00 +0900

    message

---
+++ github만들기.py
@@ -0,0 +1,26 @@
+print("Hello World")
+print("Mary's cosmetics")
+print('신씨가 소리질렀다. "도둑이야".')
+print('"C:\Windows"')
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+movie_rank = ['닥터 스트레인지','스플릿','럭키']
+movie_rank = ['닥터 스트레인지','스플릿','럭키','배트맨']
+movie_rank.insert(1, "슈퍼맨")
+print(movie_rank)
+
+nums = [1,2,3,4,5]
+print(sum(nums))
+
+cook  = ["피자","김밥","양념치킨", "족발", "피자", "김치만두", "쫄면", "소시지", "라면", "팥빙수", "김치전"]
+print(len(cook))
+
+averege = sum(nums) / len(nums)
+print(averege)
+
+print(3 == 5)
+print(3 < 5)
+
+x = 4
+print(1<x<5)
+
+print((3 == 3) and (4 != 3)) 
